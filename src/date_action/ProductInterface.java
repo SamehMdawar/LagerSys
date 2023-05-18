dca5e7def1db40c6ae2fd8e9d43ac4ce84d74fe3
Samehmdawar <Sameh.mdawar@gmail.com>
2023-05-18 14:06:03 +0200

    .

--- src/date_action/ProductInterface.java
+++ src/date_action/ProductInterface.java
@@ -1,9 +1,13 @@
 package date_action;
-
 import java.util.ArrayList;
 
 import objekten.ProductObjekt;
-
+/**
+ * Interface Klasse für "Product" Oberfläche,
+ * die in ProductAction.java verwendet
+ * @author Sam
+ *
+ */
 public interface ProductInterface {
 	
 	ArrayList<ProductObjekt>findAll();
