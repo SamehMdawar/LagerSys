59d805f505bce11c8c832aac5f74cb193853956f
Samehmdawar <Sameh.mdawar@gmail.com>
2023-05-18 15:16:50 +0200

    t

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,14 @@
 package gui;
-
+/**
+ * Main Class , Start Program
+ * @author Sam
+ *
+ */
 public class Main {
-
+/**
+ * GUI Program object anzeigen und ausführen
+ * @param args
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Window win=new Window();
